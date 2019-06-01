5b72f2dbbac13eff5af9434fd1a0558a2b307c6d
Andrew <ajo14@duke.edu>
2019-06-01 07:23:17 -0400

    initial commit

---
+++ LinkedListCreation.java
@@ -0,0 +1,18 @@
+class Solution{
+	static class Node{
+		int data;
+		Node next;
+		Node(int data, Node next){
+			this.data = data;
+			this.next = next;
+		}
+	}
+	public static void main(String[] args){
+		Node fourthNode = new Node(4, null);	  // Tail
+		Node thirdNode = new Node(3, fourthNode);
+		Node secondNode = new Node(2, thirdNode);
+		Node firstNode = new Node(1, secondNode); // Head
+	}
+}
+
+
